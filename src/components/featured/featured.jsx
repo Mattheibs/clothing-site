5836f92771834b1d58bb4a8f3c15a5dee2ae3259
Mattheibs <84919680+Mattheibs@users.noreply.github.com>
2024-06-09 15:52:56 +0200

    Featured section added

--- src/components/featured/featured.jsx
+++ src/components/featured/featured.jsx
@@ -1,10 +1,117 @@
-import React from "react";
+import React, { useState, useRef } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper/modules";
+import data from "../../data.json";
+
+// Css
+import "swiper/css/bundle";
 import "./featured.scss";
 
 //Images
+import featuredOne from "../../images/featured/featured-1.png";
+import featuredTwo from "../../images/featured/featured-2.png";
+import featuredThree from "../../images/featured/featured-3.png";
+import featuredFour from "../../images/featured/featured-4.png";
+import star from "../../images/featured/star.png";
 
 function Featured() {
-	return <div className="featured"></div>;
+	const [selectedCategory, setSelectedCategory] = useState(
+		Object.keys(data.products)[0]
+	);
+	const swiperRef = useRef(null);
+
+	const handleCategoryChange = (item) => {
+		setSelectedCategory(item);
+		if (swiperRef.current && swiperRef.current.swiper) {
+			swiperRef.current.swiper.slideToLoop(0, 0, false);
+		}
+	};
+	const imageMap = {
+		featuredOne: featuredOne,
+		featuredTwo: featuredTwo,
+		featuredThree: featuredThree,
+		featuredFour: featuredFour,
+	};
+
+	const stars = (amount) => {
+		const allStars = [];
+		for (let i = 0; i < amount; i++) {
+			allStars.push(<img src={star} key={i} alt="" />);
+		}
+		return allStars;
+	};
+
+	return (
+		<div className="featured">
+			<h2>Featured Products</h2>
+			<div className="featured__categories">
+				{Object.entries(data.products).map(([item], index) => {
+					return (
+						<button
+							key={index}
+							onClick={() => handleCategoryChange(item)}
+							className={`${
+								selectedCategory === item
+									? "active"
+									: "null"
+							} featured__categories__category`}
+						>
+							{item.slice(0, 1).toUpperCase() +
+								item.slice(1)}
+						</button>
+					);
+				})}
+			</div>
+
+			<Swiper
+				ref={swiperRef}
+				spaceBetween={40}
+				slidesPerView={4}
+				loop={true}
+				pagination={{
+					clickable: true,
+				}}
+				modules={[Pagination]}
+			>
+				{/*  */}
+				{data.products[selectedCategory].map((item, index) => {
+					return (
+						<SwiperSlide key={index}>
+							<div className="featured__slider-top">
+								<img
+									className="featured__slider-top__person"
+									src={imageMap[item.img]}
+									alt=""
+								/>
+								<div>{stars(item.amountOfStars)}</div>
+							</div>
+
+							<h4>{item.header}</h4>
+							<p className="explanation">
+								{item.explanation}
+							</p>
+							<h4>{item.cost}</h4>
+							<div className="color-options">
+								{item.colorOptions.map(
+									(item, index) => {
+										return (
+											<div
+												key={index}
+												style={{
+													backgroundColor: `${item}`,
+												}}
+												className="color-options__option"
+											></div>
+										);
+									}
+								)}
+							</div>
+						</SwiperSlide>
+					);
+				})}
+			</Swiper>
+		</div>
+	);
 }
 
 export default Featured;
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "../../components/hero/hero";
+import Featured from "../../components/featured/featured";
 
 function Home() {
 	return (
 		<div className="home">
 			<Hero />
+			<Featured />
 		</div>
 	);
 }
